ba5c11f94458c0c0e7dedc4b0c3044b0daf78b22
x1 <sezermehmed98@gmail.com>
2022-05-26 12:03:31 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpanseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpanseItem></ExpanseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpanseItem.css'
+function ExpanseItem() {
+    const expenseDate = new Date(2022,5,24);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.76;
+    return (
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default ExpanseItem;
