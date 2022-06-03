c971f29a5086e61f6e522d0e65784c5f12985e1d
Sezer <sezermehmed98@gmail.com>
2022-06-03 17:58:38 +0300

    third

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
-import './ExpanseItem.css'
-function ExpanseItem(props) {
-    const month = props.date.toLocaleString('en-US', { month: 'long' });
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    const year = props.date.getFullYear();
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+function ExpenseItem(props) {
+    // const month = props.date.toLocaleString('en-US', { month: 'long' });
+    // const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    // const year = props.date.getFullYear();
     return (
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div> 
-            </div>
-         
+           <ExpenseDate date={props.date}/>       
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
@@ -20,4 +16,4 @@ function ExpanseItem(props) {
     )
 }
 
-export default ExpanseItem;
+export default ExpenseItem;
