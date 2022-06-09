c28a1eafcab054da302d67bfe49e45cdd8e3f1de
Sezer <sezermehmed98@gmail.com>
2022-06-09 19:05:51 +0300

    9 june

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 import './Card.css';
-function Card() {
+function Card(props) {
+    const classes = 'card ' + props.className;
 
-
-    return <div className="card"></div>
+    return <div className={classes}>{props.children}</div>
 }
 
 export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+import Card from './Card';
 function ExpenseItem(props) {
     // const month = props.date.toLocaleString('en-US', { month: 'long' });
     // const day = props.date.toLocaleString('en-US', { day: '2-digit' });
